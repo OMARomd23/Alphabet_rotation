6750471c26a8586186106c3b9470c8b3749f3401
OMAR <63082365+OMARomd23@users.noreply.github.com>
2024-03-19 23:16:11 +0000

    Update Alphabet_rot.py

--- Alphabet_rot.py
+++ Alphabet_rot.py
@@ -1,22 +1,53 @@
 text=input("Put your text here:  ")
+print(f'old: {text}')
+
 alpha= list("abcdefghijklmnopqrstuvwxyz")
+
 alphadict=dict([(alpha[alpha.index(x)],alpha.index(x)) for x in alpha])
+
 def rot13(text):
+
     global alphadict
+
     global alpha
-    rot=int(input("Enter teh rotation number: "))
+
+    rot=int(input("Enter the rotation number: "))
+
     cipher=[]
+    
     for i in text:
+
         if i.lower() not in alpha:
+
             cipher.append(i)
+            
         else:
+
             k=alphadict[i.lower()]+rot
+
             if k > 25:
+
                 k=k%26
+
                 cipher.append(alpha[k])
+
             elif k < 0:
-                k=26-abs(k)
+
+                if abs(k) > 25:
+
+                    j=(abs(k)%26)-1
+
+                    if j < 0:k=(25+abs(j))%26
+
+                    else:k=25-j
+
+                else:k=25+k
+
                 cipher.append(alpha[k])
+
             else: cipher.append(alpha[k])
+
     return ''.join(cipher)
-print(rot13(text))
+
+print(f'new: {rot13(text)}')
+
